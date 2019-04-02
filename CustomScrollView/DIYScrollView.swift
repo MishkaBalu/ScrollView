4cf9f493825c3b775ad3c57ccd76af936d9e5645
Leonid Shkolnyk <duolts1997@gmail.com>
2019-04-02 18:34:35 +0300

    create gesture recognizer and scroll view

---
+++ CustomScrollView/DIYScrollView.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class DIYScrollView: UIView {
+
+  @IBAction func handlePan(_ recognizer: UIPanGestureRecognizer) {
+    let translation = recognizer.translation(in: self)
+    bounds.origin.y = bounds.origin.y - translation.y
+    
+    recognizer.setTranslation(CGPoint.zero, in: self)
+  }
+
+}
